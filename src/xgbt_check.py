ce9f5c461e987b7b7bd32cf9aaf8a83d22ac7d50
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-24 14:23:14 -0700

    first try for xgbt started

--- src/xgbt_check.py
+++ src/xgbt_check.py
@@ -4,7 +4,7 @@ __author__ = 'Vladimir Iglovikov'
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
-import xgboost as xgb
+# import xgboost as xgb
 from sklearn.cross_validation import ShuffleSplit
 from sklearn.metrics import mean_squared_error
 import math
@@ -23,24 +23,71 @@ joined['end_flag'] = joined.apply(lambda x: flag(x), 1)
 
 #rename forming a and x to 1 and 0
 joined['forming_a'] = joined['forming_a'].map({'Yes': 1, 'No': 0})
+joined['forming_x'] = joined['forming_x'].map({'Yes': 1, 'No': 0})
 
 #use label encoder on ends
-
 le = LabelEncoder()
 le.fit(np.hstack([joined['end_a'].values, joined['end_x'].values]))
 
 joined['end_x'] = le.transform(joined['end_x'].values)
 joined['end_a'] = le.transform(joined['end_a'].values)
 
+#use label encoder on supplier
+le = LabelEncoder()
+joined['supplier'] = le.fit_transform(joined['supplier'].values)
+
+#map bracket pricing to Yes=1, No=0
+joined['bracket_pricing'] = joined['bracket_pricing'].map({'Yes': 1, 'No': 0})
+
 train = joined[joined['id'] == -1]
 test = joined[joined['cost'] == -1]
 
-features = list(train.columns)
-print features
+features = ['annual_usage',
+            'bracket_pricing',
+            # 'cost',
+            # 'id',
+            'min_order_quantity',
+            'quantity',
+            # 'quote_date',
+            'supplier',
+            # 'tube_assembly_id',
+            # 'component_id_1',
+            'quantity_1',
+            # 'component_id_2',
+            'quantity_2',
+            # 'component_id_3',
+            'quantity_3',
+            # 'component_id_4',
+            'quantity_4',
+            # 'component_id_5',
+            'quantity_5',
+            # 'material_id',
+            'diameter',
+            'wall',
+            'length',
+            'num_bends',
+            'bend_radius',
+            # 'end_a_1x',
+            # 'end_a_2x',
+            # 'end_x_1x',
+            # 'end_x_2x',
+            'end_a',
+            'end_x',
+            'num_boss',
+            'num_bracket',
+            # 'other',
+            'forming_a',
+            'forming_x',
+            # 'spec',
+            '0039', '0038', '0035', '0037', '0036', '0030', '0033', '0014', '0066', '0067', '0064', '0065', '0062', '0063', '0060', '0061', '0068', '0069', '0004', '0049', '0006', '0007', '0001', '0002', '0003', '0040', '0042', '0043', '0044', '0009', '0005', '0047', '0019', '0054', '0071', '0070', '0073', '0072', '0075', '0074', '0077', '0076', '0079', '0078', '0017', '0016', '0059', '0058', '0013', '0012', '0011', '0010', '0053', '0052', '0051', '0050', '0057', '0056', '0055', '0018', '0088', '0084', '0085', '0086', '0087', '0080', '0081', '0082', '0083', '0022', '0023', '0020', '0021', '0026', '0027', '0024', '0025', '0028', '0029', '0046', '0091', '0096', '0094', '0092', '0015',
+            'end_flag'
+            ]
 
 y = train['cost'].apply(lambda x: math.log(x + 1), 1)
 
-X = train.drop(['id', 'spec', 'tube_assembly_id', 'quote_date'], 1)
+X = train[features]
+# print X.info()
+# print X.head9()
 
 X.fillna(-1, inplace=True)
 params = {
